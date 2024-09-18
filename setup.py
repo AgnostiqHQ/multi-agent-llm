3b4db2e2934ff964f5cdf63f7a9e56ac888207cb
Ara Ghukasyan <ara@agnostiq.ai>
2024-09-18 11:39:25 -0400

    version 0.1.1; update setup.py with readme text; auto-read reqs; and more classifiers

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
+from pathlib import Path
 from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+
+# Read text from README.md
+long_description = (this_directory / "README.md").read_text()
+
+# Read list of requirements
+install_requires = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
     name="multi_agent_llm",
-    version="0.1",
-    description="LLM Multi-Agent implementation.",
-    author="Agnostiq Inc",
+    version="0.1.1",
+    description="An LLM Multi-Agent framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Agnostiq Inc.",
     author_email="contact@agnostiq.ai",
     packages=find_packages(),
-    install_requires=["openai>=1.46.0", "pydantic"],
+    install_requires=install_requires,
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3 :: Only",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires=">=3.7",
 )
